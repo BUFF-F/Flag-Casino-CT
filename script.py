23ce2887301377ea6a0ce8d2046efd8587267c50
Bufff <friouia10@gmail.com>
2025-11-08 03:16:34 +0100

    final product

--- script.py
+++ script.py
@@ -1,15 +1,32 @@
-data=[]
+from data import data
+from lut import lut
 
-LUT = ""
 lut_d = {}
 
 lut = lut.split("\n")
+
+lut_entries = []
 for elem in lut:
-    elem = elem.split(":")
-    lut_d[int(elem[1], 16)] = chr(int(elem[0]))
+    parts = elem.split(":")
+    lut_entries.append((int(parts[0]), parts[1].lower()))
+
+lut_d = {int(v, 16): chr(k) for k, v in lut_entries}
 
 flag = ""
+
 for i in data:
-    flag += lut_d[i]
+    s = i.lower()
+    key = int(s, 16)
+    if key in lut_d:
+        ch = lut_d[key]
+    else:
+        ch = None
+        for k, v in lut_entries:
+            if v.endswith(s):
+                ch = chr(k)
+                break
+        if ch is None:
+            raise KeyError(f"No mapping for {i}")
+    flag += ch
 
 print(flag)
